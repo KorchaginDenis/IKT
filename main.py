35ec6e33567b54064e2661d0df48dad8a15cc552
KorchaginDenis <120161335+KorchaginDenis@users.noreply.github.com>
2022-12-18 15:47:36 +0300

    Добавление nginx and Docker compose

--- main.py
+++
@@ -1,7 +0,0 @@
-import uvicorn
-from app.api import app
-
-#http://127.0.0.1:8000 и http://127.0.0.1:8000/items
-if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000, log_level='info')
-
